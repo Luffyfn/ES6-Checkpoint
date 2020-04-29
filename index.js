91f4271f824db775f89e6ddac9a1239e31cfc227
Luffyfn <faresnasfi@gmail.com>
2020-04-29 13:14:37 +0100

    Checkpoint done!
    ES5 Functions => ES6 Functions
    ES5 String quotes => ES6 String quotes
    ES5 For loops => ES6 For Loops
    ES5 Variables declaration => ES6 Variables declaration

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let pets = [
-  { name: "Max", type: "dog", bornOn: 2018 },
-  { name: "Angel", type: "cat", bornOn: 2015 },
-  { name: "Jasper", type: "dog", bornOn: 2016 }
+  { name: `Max`, type: `dog`, bornOn: 2018 },
+  { name: `Angel`, type: `cat`, bornOn: 2015 },
+  { name: `Jasper`, type: `dog`, bornOn: 2016 }
 ];
 
 getAge = pet => new Date().getFullYear() - pet.bornOn;
 
 
-let petsWithAge = pets.map(age => Object.assign(a, {age: getAge(age)}));
+let petsWithAge = pets.map(age => Object.assign(age, {age: getAge(age)}));
 
-let dogs = petsWithAge.filter(dog => dog.type === "dog");
+let dogs = petsWithAge.filter(dog => dog.type === `dog`);
 
-let jasper = petsWithAge.find(j => j.name === "Jasper");
-console.log("Jasper is " + jasper.age + " years old");
+let jasper = petsWithAge.find(j => j.name === `Jasper`);
+console.log(`Jasper is ` + jasper.age + ` years old`);
